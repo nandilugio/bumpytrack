1c4c58e5d847903367167e16ec0ee02dd31e01c9
Fernando Stecconi <nandilugio@gmail.com>
2020-03-06 16:04:36 +0100

    Tyding up tests

--- tests/bumpytrack_test.py
+++ tests/bumpytrack_test.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import codecs
 import contextlib
 import os
 import shutil
@@ -12,9 +11,11 @@ else:
     import subprocess
 
 import pytest
-import six
 
 import bumpytrack
+
+
+# Utils ########################################################################
 
 
 @contextlib.contextmanager
@@ -25,26 +26,54 @@ def cwd_at(path):
     finally: os.chdir(original_wd)
 
 
+def run(command, assert_success=True):
+    completed_process = subprocess.run(
+        command,
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+
+    if assert_success:
+        assert completed_process.returncode == 0
+
+    return completed_process
+
+
+# Fixtures #####################################################################
+
+
 @pytest.fixture
 def project_context(tmpdir):
     project_path = tmpdir
-    config_path = project_path.join("pyproject.toml")
-    replaceable_file_path = project_path.join("replaceable.txt")
-    shutil.copyfile("tests/data/integration_tests_pyproject.toml", str(config_path))
-    shutil.copyfile("tests/data/replaceable.txt", str(replaceable_file_path))
+    project_path_str = str(project_path)
+
+    config_path_str = str(project_path.join("pyproject.toml"))
+    replaceable_file_path_str = str(project_path.join("replaceable.txt"))
+    source_file_path_str = str(project_path.join("source.txt"))
+
+    shutil.copyfile("tests/data/integration_tests_pyproject.toml", config_path_str)
+    shutil.copyfile("tests/data/replaceable.txt", replaceable_file_path_str)
+    shutil.copyfile("tests/data/source.txt", source_file_path_str)
+
+    with cwd_at(project_path_str):
+        run("git init")
+        run("git add .")
+        run("git commit -m 'Initial commit.'")
 
     return {
-        "project_path": str(project_path),
-        "config_path": str(config_path),
-        "replaceable_file_path": str(replaceable_file_path),
+        "project_path": project_path_str,
+        "config_path": config_path_str,
+        "replaceable_file_path": replaceable_file_path_str,
+        "source_file_path": source_file_path_str,
     }
 
 
 @pytest.fixture
 def fail_mocking(mocker):
-    FailCalled = type("FailCalled", (RuntimeError,), {})
+    class FailCalled(RuntimeError): pass
 
-    def fail_called(*args, **kwargs):
+    def fail_called(*_args, **_kwargs):
         raise FailCalled()
 
     def stopping_at_fail(callback):
@@ -57,47 +86,64 @@ def fail_mocking(mocker):
 
     fail_mock = mocker.patch("bumpytrack.fail", side_effect=fail_called)
 
-    return (fail_mock, stopping_at_fail)
+    return fail_mock, stopping_at_fail
+
+
+# Feature/Integration Tests ####################################################
 
 
 def test_cli_info():
-    completed_process = subprocess.run(["bumpytrack", "-h"])
-    assert completed_process.returncode == 0
-    
-    completed_process = subprocess.run(
-        ["bumpytrack", "--version"],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT
-    )
-    assert completed_process.returncode == 0
-    assert completed_process.stdout.strip() == b"Version: 1.1.3"  # Replaced by bumpytrack itself
+    completed_process = run("bumpytrack -h")
+    assert "usage:" in completed_process.stdout
+
+    completed_process = run("bumpytrack --version")
+    # Add stdout and stderr not to restrict where the version is actually written
+    process_output = completed_process.stdout.strip() + completed_process.stderr.strip()
+    assert process_output == b"Version: 1.1.3"  # Replaced by bumpytrack itself
 
 
-def test_integration(project_context):
+def test_bump_replaces_version_in_files(project_context):
     with cwd_at(project_context["project_path"]):
-        completed_process = subprocess.run(
-            [
-                "bumpytrack", "minor",
-                "--no-git-commit", "--no-git-tag",
-                "--config-path", project_context["config_path"]
-            ],
-            stdout=subprocess.PIPE
+        completed_process = run(
+            "bumpytrack minor --no-git-commit --no-git-tag --config-path " + project_context["config_path"]
         )
-        
-        assert completed_process.returncode == 0
         assert completed_process.stdout.strip() == \
-            b"Current version: '1.2.3'\n" \
-            b"New version: '1.3.0'\n" \
-            b"Replacing version srting in files"
+               b"Current version: '1.2.3'\n" \
+               b"New version: '1.3.0'\n" \
+               b"Replacing version srting in files"
+        with open(project_context["config_path"], "rb") as f:
+            config_file_contents = f.read()
+            assert b"1.3.0" in config_file_contents
         with open(project_context["replaceable_file_path"], "rb") as f:
             replaceable_file_contents = f.read()
             assert b"1.3.0" in replaceable_file_contents
-            assert b"\xc3\xa1\xc3\xa8\xc4\xa9\xc3\xb4\xc3\xbc" in replaceable_file_contents  # UTF-8 for áèĩôü
+            assert b"\xc3\xa1\xc3\xa8\xc4\xa9\xc3\xb4\xc3\xbc" in replaceable_file_contents  # UTF-8 for "áèĩôü"
+
+
+def test_bump_commits_and_tags_repo(project_context):
+    with cwd_at(project_context["project_path"]):
+        completed_process = run("git log --oneline")
+        assert b"Bumping version" not in completed_process.stdout
+
+        run("bumpytrack patch --git-commit --git-tag --config-path " + project_context["config_path"])
+
+        completed_process = run("git log --oneline")
+        assert b"Bumping version: 1.2.3 \xe2\x86\x92 1.2.4" in completed_process.stdout  # UTF-8 for "→"
+
+        completed_process = run("git describe --tags --abbrev=0")
+        assert completed_process.stdout.strip() == b"v1.2.4"
+
+
+# Unit Tests ###################################################################
 
 
 def test_version_incrementing(fail_mocking):
     fail_mock, stopping_at_fail = fail_mocking
-    
+
+    fail_mock.assert_not_called()
+    stopping_at_fail(bumpytrack.increment_version)("1.2.3", "not_a_valid_part")
+    fail_mock.assert_called_once_with("Part should be one of: major, minor or patch.")
+
     assert stopping_at_fail(bumpytrack.increment_version)("1.2.3", "major") == "2.0.0"
     assert stopping_at_fail(bumpytrack.increment_version)("1.2.3", "minor") == "1.3.0"
     assert stopping_at_fail(bumpytrack.increment_version)("1.2.3", "patch") == "1.2.4"
